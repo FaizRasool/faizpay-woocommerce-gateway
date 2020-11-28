3a21843e0351abbaf5d5a7f52aac78796489c5e6
Faiz Rasool <faiz.rasool@stablelogic.com>
2020-11-28 13:30:46 +0000

    1. fix logo

--- FaizPay/CheckOutUI.php
+++ FaizPay/CheckOutUI.php
@@ -8,21 +8,31 @@ class CheckOutUI
 {
     public static function get()
     {
-        $button = plugin_dir_url(dirname(__FILE__, 1)) . '/assets/pay-button.png';
+        $button = plugin_dir_url(dirname(__FILE__, 1)) . 'assets/pay-button.png';
         ?>
-            <img src="<?= $button; ?>"
-                 id="fp-image"/>
+        <img src="<?= $button; ?>"
+             id="fp-image"/>
         <style>
             .payment_method_faizpay_payment {
                 background: transparent !important;
                 padding: 0px !important;
+                width: 100% !important;
+                height: 170px !important;
+                max-height: 170px !important;
+                display: block !important;
+                min-height: 170px !important;
             }
 
             #fp-image {
-                max-width: 250px;
-                background: transparent;
-                min-width: 250px;
-                width: 250px;
+                max-width: 250px !important;
+                background: transparent !important;
+                min-width: 250px !important;
+                width: 250px !important;
+                height: 150px !important;
+                max-height: 150px !important;
+                display: block !important;
+                min-height: 150px !important;
+                float: none !important;
             }
         </style>
         <?php
