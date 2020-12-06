a515f3f6991ada67f45304ab98f44d560e31ee87
Faiz Rasool <faiz.rasool@stablelogic.com>
2020-12-06 22:33:23 +0000

    1. updated the checkout UI for the plugin

--- FaizPay/AdminPortalOptions.php
+++ FaizPay/AdminPortalOptions.php
@@ -4,8 +4,6 @@
 namespace FaizPay;
 
 
-use Ramsey\Uuid\Uuid;
-
 class AdminPortalOptions
 {
 
@@ -32,20 +30,42 @@ class AdminPortalOptions
                 'description' => 'Enter the terminal Secret Here',
                 'default' => '',
                 'desc_tip' => true,
-            )
+            ),
+            'title' => array(
+                'title' => 'Title',
+                'type' => 'text',
+                'description' => 'This controls the title which the user sees during checkout.',
+                'default' => 'LLoyds, Barclays, Starling and other banks',
+                'desc_tip' => true,
+            ),
+            'description' => array(
+                'title' => 'Description',
+                'type' => 'text',
+                'desc_tip' => true,
+                'description' => 'This controls the description which the user sees during checkout.',
+                'default' => 'Pay instantly via online bank transfer',
+            ),
         );
     }
 
     public static function validate($terminal_secret, $terminal_id)
     {
-        if (!Uuid::isValid($terminal_secret)) {
+        if (!self::validateUUID($terminal_secret)) {
             \WC_Admin_Settings::add_error('Invalid Terminal Secret Given');
             return false;
         }
 
 
-        if (!Uuid::isValid($terminal_id)) {
+        if (!self::validateUUID($terminal_id)) {
             \WC_Admin_Settings::add_error('Invalid Terminal ID Given');
+            return false;
+        }
+        return true;
+    }
+
+    private static function validateUUID($uuid)
+    {
+        if (!is_string($uuid) || (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) !== 1)) {
             return false;
         }
         return true;
--- FaizPay/FaizPayPaymentGateway.php
+++ FaizPay/FaizPayPaymentGateway.php
@@ -13,10 +13,14 @@ final class FaizPayPaymentGateway extends WC_Payment_Gateway
     {
         $this->id = 'faizpay_payment';
         $this->method_title = 'FaizPay';
-        $this->title = 'FaizPay';
+
         $this->method_description = "Fast instant bank to bank payments";  // to backend
-        $this->description = "Pay via your bank account";                  // to the front end portal
-        $this->has_fields = true;
+        $this->order_button_text = 'Proceed to FaizPay';
+
+        $this->title = $this->get_option('title');
+        $this->description = $this->get_option('description');
+
+        $this->has_fields = false;
 
         // only support products
         $this->supports = array(
@@ -62,9 +66,16 @@ final class FaizPayPaymentGateway extends WC_Payment_Gateway
         return PaymentProcess::process($order_id, $this->terminal_id, $this->terminal_secret);
     }
 
-    public function payment_fields()
+    public function get_icon()
     {
-        CheckOutUI::get();
+        $icon_html = '';
+        $providers = array('barclays', 'hsbc', 'lloyds', 'monzo',
+            'nationwide', 'starling', 'halifax', 'natwest', 'santander');
+        foreach ($providers as $provider) {
+            $url = \WC_HTTPS::force_https_url(plugin_dir_url(dirname(__FILE__, 1)) . 'assets/' . $provider . '.svg');
+            $icon_html .= '<img width="26" src="' . esc_attr($url) . '" alt="' . esc_attr($provider) . '" />';
+        }
+        return apply_filters('woocommerce_gateway_icon', $icon_html, $this->id);
     }
 
     public function webhook()
